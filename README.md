57a7befdf5567f72c022cc8e0951839ce1f532ae
Anshul Goyal <anshulgoel151999@gmail.com>
2018-08-06 13:26:12 +0530

    added https suport and file base data store

---
+++ README.md
@@ -0,0 +1,2 @@
+#LibLessServer
+This is attempt to write a Nodejs Server without any npm module and with out any framework.Comments and suggestions are most welcomed .
--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const http = require('http');
+const https=require('https');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const fs=require('fs');
+
+
+
+const serverHttp = http.createServer((req,res)=>{
+    serverLogic(req,res);
+})
+
+options={
+    'key':fs.readFileSync('./https/key.pem'),
+    'cert':fs.readFileSync('./https/cert.pem'),
+}
+const serverHttps=https.createServer(options,(req,res)=>{
+    serverLogic(req,res)
+})
 
 
 
 
-const server = http.createServer((req, res) => {
+
+
+
+serverHttp.listen(4443, () => {
+    console.log('server started at port 4443');
+})
+
+serverHttps.listen(3000, () => {
+    console.log('server started at port 3000');
+})
+handleNotFound=(data,cb)=>{
+    cb(404,{});
+}
+handler={};
+
+handler['sample']=function(data={},cb=()=>{}){
+    cb(200,{'name':'anshul'});
+}
+
+serverLogic=(req, res) => {
     const parserdUrl = url.parse(req.url, true);
     const trimedUrl = parserdUrl.pathname.replace(/^\/|\/$/g, "");
     const {headers,method} = req;
@@ -25,25 +60,4 @@ const server = http.createServer((req, res) => {
              res.end(JSON.stringify(json));
         })
     })
-})
-
-
-
-
-
-
-
-
-
-server.listen(3000, () => {
-    console.log('server started at port 3000');
-})
-
-handleNotFound=(data,cb)=>{
-    cb(404,{});
-}
-handler={};
-
-handler['sample']=function(data={},cb=()=>{}){
-    cb(200,{'name':'anshul'});
 }
