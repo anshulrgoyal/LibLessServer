fc65395d2382bac33dbe7e4137b6c377c7f30ff2
Anshul Goyal <anshulgoel151999@gmail.com>
2018-08-23 23:29:31 +0530

    made a seperate directory for server

---
+++ server/lib/helper.js
@@ -0,0 +1,54 @@
+'use strict'
+const crypto = require('crypto');
+
+let lib = {};
+// time in hours,payload is an object
+lib.createToken = (payload, time) => {
+    const head = Buffer.from(JSON.stringify({
+        "alg": "HS256",
+        "typ": "JWT"
+    })).toString('base64').replace(/\-/g, "+").replace(/_/g, "/");
+    const pay = Buffer.from(JSON.stringify({ payload, time: time * 1000 * 60 * 60 + Date.now() })).toString('base64').replace(/\-/g, "+").replace(/_/g, "/");
+
+    const sign = crypto.createHmac('SHA256', 'some').update(head + '.' + pay).digest('base64').replace(/\-/g, "+").replace(/_/g, "/");
+    return head + '.' + pay + '.' + sign;
+}
+
+lib.verify = (token, cb) => {
+    const [head, pay, sign] = token.split('.');
+    const verifySign = crypto.createHmac('SHA256', 'some').update(head + '.' + pay).digest('base64').replace(/\-/g, "+").replace(/_/g, "/");
+    if (sign == verifySign) {
+        let payload = pay.replace(/\+/g, "-").replace(/\//g, "_");
+        payload = Buffer.from(payload, 'base64').toString('utf-8');
+        payload = JSON.parse(payload);
+        if (payload.time > Date.now()) {
+            cb(null, payload.payload);
+        }
+        else {
+            cb('token expired', null);
+        }
+    }
+    else {
+        cb('token invalid', null);
+    }
+}
+lib.refresh = (token, cb) => {
+    const [head, pay, sign] = token.split('.');
+    const verifySign = crypto.createHmac('SHA256', 'some').update(head + '.' + pay).digest('base64').replace(/\-/g, "+").replace(/_/g, "/");
+    if (sign == verifySign) {
+        let payload = pay.replace(/\+/g, "-").replace(/\//g, "_");
+        payload = Buffer.from(payload, 'base64').toString('utf-8');
+        payload = JSON.parse(payload);
+        if (payload.time < Date.now()) {
+            cb(null, lib.createToken(payload.payload, 6));
+        }
+        else {
+            cb('token not expired', null);
+        }
+    }
+    else {
+        cb('token invalid', null);
+    }
+}
+
+module.exports = lib;
